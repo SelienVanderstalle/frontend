40ab1464d70399823492d8079e24993ba310be96
SelienVanderstalle <94372572+SelienVanderstalle@users.noreply.github.com>
2021-12-08 08:17:28 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
 // JavaScript Document
+var hamburgerBtn = document.querySelector('header nav section:nth-of-type(1)');
+
+hamburgerBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    var body = document.querySelector('body');
+    body.classList.toggle('toonMenu');
+}
